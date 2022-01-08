fb7a46aea973207d99b3b6f2d426fd79c319c732
Ren Wang <iamwrm@gmail.com>
2022-01-09 00:20:57 +0800

    fix trailing space problem

--- ci/prepare_ubuntu20.sh
+++ ci/prepare_ubuntu20.sh
@@ -1,10 +1,10 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update 
-apt-get install -y \ 
-    gcc \ 
-    make \ 
-    python3 \ 
-    python3-pip \ 
-    ninja-build \ 
+apt-get install -y \
+    gcc \
+    make \
+    python3 \
+    python3-pip \
+    ninja-build \
     cmake 
 pip install conan
