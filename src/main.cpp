60c4ad258137c71a059b47dfccbfb378d1328849
Ren Wang <iamwrm@gmail.com>
2022-01-09 13:19:29 +0800

    add more benches

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <benchmark/benchmark.h>
-
 #include "math_op.hpp"
+
+#include <benchmark/benchmark.h>
 
 #define BENCH_OP(op)                                                 \
     static void BM_##op(benchmark::State& state)                     \
@@ -12,5 +12,11 @@
     BENCHMARK(BM_##op);
 
 BENCH_OP(add);
+BENCH_OP(minus);
+BENCH_OP(multiply);
+BENCH_OP(divide);
+BENCH_OP(log);
+BENCH_OP(pow);
+BENCH_OP(exp);
 
 BENCHMARK_MAIN();
