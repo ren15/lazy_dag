689dbd57da6d8808f3a9fdd95ff2b084b456b857
Ren Wang <iamwrm@gmail.com>
2022-01-20 15:05:36 +0000

    more naive benches

--- src/ranges_impl/bench.cpp
+++ src/ranges_impl/bench.cpp
@@ -10,7 +10,7 @@
 
 static void BM_v_add_s(benchmark::State& state)
 {
-    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 1001LL);
+    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 100001LL);
     for (auto _ : state) {
         auto v3 = v2 + 2LL;
         benchmark::DoNotOptimize(v3);
@@ -20,7 +20,7 @@ BENCHMARK(BM_v_add_s);
 
 static void BM_v_add_2s(benchmark::State& state)
 {
-    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 1001LL);
+    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 100001LL);
     for (auto _ : state) {
         auto v3 = v2 + 2LL + 3LL;
         benchmark::DoNotOptimize(v3);
@@ -30,8 +30,8 @@ BENCHMARK(BM_v_add_2s);
 
 static void BM_v_add_v(benchmark::State& state)
 {
-    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 1001LL);
-    auto v3 = RangeImpl::Vec<long long>::iota(1LL, 1001LL);
+    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 100001LL);
+    auto v3 = RangeImpl::Vec<long long>::iota(1LL, 100001LL);
     for (auto _ : state) {
         auto v4 = v2 + v3;
         benchmark::DoNotOptimize(v4);
@@ -39,14 +39,25 @@ static void BM_v_add_v(benchmark::State& state)
 }
 BENCHMARK(BM_v_add_v);
 
-static void BM_v2(benchmark::State& state)
+static void BM_v_multiply_v(benchmark::State& state)
 {
-    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 1001LL);
+    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 100001LL);
+    auto v3 = RangeImpl::Vec<long long>::iota(1LL, 100001LL);
+    for (auto _ : state) {
+        auto v4 = v2 * v3;
+        benchmark::DoNotOptimize(v4);
+    }
+}
+BENCHMARK(BM_v_multiply_v);
+
+static void BM_sum(benchmark::State& state)
+{
+    auto v2 = RangeImpl::Vec<long long>::iota(1LL, 100001LL);
     for (auto _ : state) {
         auto sum_value = v2.sum();
         benchmark::DoNotOptimize(sum_value);
     }
 }
-BENCHMARK(BM_v2);
+BENCHMARK(BM_sum);
 
 BENCHMARK_MAIN();
