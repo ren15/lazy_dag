d09fabc844bccba8501140c497e44db9948fa304
Ren Wang <iamwrm@gmail.com>
2022-05-13 19:38:49 +0800

    Move range-v3 to conan

--- ci/dep_build.sh
+++ ci/dep_build.sh
@@ -10,12 +10,12 @@ cmake --build "build" --config Release -j
 cd build
 cmake --install . --prefix ${CMAKE_PREFIX_DIR} 
 
-# setup range-v3
-cd ${DEP_DIR}/range-v3
-cmake -E make_directory "build"
-cmake -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_DIR} \
-    -S . -B "build"
-cmake --build "build" -j
-cd build
-cmake --install . --prefix ${CMAKE_PREFIX_DIR} 
+# # setup range-v3
+# cd ${DEP_DIR}/range-v3
+# cmake -E make_directory "build"
+# cmake -DCMAKE_BUILD_TYPE=Release \
+#     -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_DIR} \
+#     -S . -B "build"
+# cmake --build "build" -j
+# cd build
+# cmake --install . --prefix ${CMAKE_PREFIX_DIR} 
--- ci/dep_download.sh
+++ ci/dep_download.sh
@@ -1,5 +1,5 @@
 mkdir -p ${DEP_DIR}
 cd ${DEP_DIR}
 git clone https://github.com/google/benchmark.git
-git clone https://github.com/ericniebler/range-v3.git
+# git clone https://github.com/ericniebler/range-v3.git
 git clone https://github.com/asavine/CompFinance.git
